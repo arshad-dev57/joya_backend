7efdb0ac2085084aeb45ca32e5d6a383c41546d7
Arshad Nasir <qureshiarshad947@gmail.com>
2025-09-02 13:38:23 +0500

    dfsdt

--- routes/review_routes.js
+++ routes/review_routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/review_controller');
 const { protect } = require('../middlewares/authmiddleware');
-
+const { protect2 } = require('../middlewares/oldauthmiddleware');
 // Create review
-router.post('/create', protect, reviewController.addReview);
+router.post('/create', protect2, reviewController.addReview);
 
 // Get vendor reviews
 router.get('/:vendorId', reviewController.getVendorReviews);
